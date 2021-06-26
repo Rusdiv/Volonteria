af7e56e42714aad7ed1c5ac4a7c3264d330f250e
Rusdiv <ruslan.ru226@mail.ru>
2021-06-26 11:40:04 +0300

    added preloader and getting news when u go on site

---
+++ components/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <React.Fragment>
+      <img src="https://pear-advert.ru/images/uploads/blog/273/39624707_60x60.gif" />
+    </React.Fragment>
+  );
+};
+
+export default Preloader;
