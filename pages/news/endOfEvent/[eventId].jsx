d57094824ca833853520ed5140b1b210bd729e1c
Rusdiv <ruslan.ru226@mail.ru>
2021-09-08 22:43:41 +0300

    fix ending event

--- pages/news/endOfEvent/[eventId].jsx
+++ pages/news/endOfEvent/[eventId].jsx
@@ -1,21 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import AuthContext from '../../../store/auth-context';
 
 export default function endOfEvent({ eventId }) {
-  return <div>{eventId}</div>;
+  const { userData } = useContext(AuthContext);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setData(
+        await axios.post(
+          `http://t0toro-wordpress.tw1.ru/wp-json/vl/v1/end/event/${eventId}`,
+          {
+            event_id: eventId,
+            user_id: userData.id,
+          },
+        ).data,
+      );
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {eventId}
+      <p>
+        {data === 'U were not registrated on this event' ? (
+          <div>Вы не зарегестрированны</div>
+        ) : (
+          <div>Смотреть нетворк</div>
+        )}
+      </p>
+    </div>
+  );
 }
 
 export const getServerSideProps = async (context) => {
-  const { userData } = useContext(AuthContext);
   const { eventId } = context.params;
-  axios.post(
-    `http://t0toro-wordpress.tw1.ru/wp-json/vl/v1/end/event${eventId}`,
-    {
-      event_id: eventId,
-      user_id: userData.id,
-    },
-  );
+
   return {
     props: {
       eventId,
