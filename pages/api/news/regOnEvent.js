ea3a2b7a01fbb0d8e7b060febc4d41f705c2cb65
Rusdiv <ruslan.ru226@mail.ru>
2021-09-12 18:16:14 +0300

    #master added form data

--- pages/api/news/regOnEvent.js
+++ pages/api/news/regOnEvent.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import FormData from 'form-data';
 
 require('dotenv').config();
 
 export default async (req, res) => {
+  const formData = new FormData();
+  formData.append('user_id', req.body.user_id);
+  formData.append('event_id', req.body.newsId);
   const { data } = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
-    {
-      user_id: req.body.user_id,
-      event_id: req.body.newsId,
-    },
+    formData,
     {
       headers: {
         'Content-Type': 'multipart/form-data',
