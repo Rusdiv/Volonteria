06ad5e0088b91a0551f4c3c48aa3f59c83a84fc5
Rusdiv <ruslan.ru226@mail.ru>
2021-09-12 18:20:49 +0300

    #master logging

--- pages/api/news/regOnEvent.js
+++ pages/api/news/regOnEvent.js
@@ -7,17 +7,12 @@ export default async (req, res) => {
   const formData = new FormData();
   formData.append('user_id', req.body.user_id);
   formData.append('event_id', req.body.newsId);
-  const { data } = await axios.post(
+  const request = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
     formData,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    },
   );
 
-  console.log(data);
+  console.log(formData, request);
 
-  res.json(data);
+  res.json(1);
 };
