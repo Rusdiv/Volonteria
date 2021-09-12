b0689d0d3ec5344e82270e20e0a89e6cb1dd7ed5
Rusdiv <ruslan.ru226@mail.ru>
2021-09-12 12:23:21 +0300

    as

--- pages/api/news/endOnEvent.js
+++ pages/api/news/endOnEvent.js
@@ -6,10 +6,9 @@ export default async (req, res) => {
   const { data } = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/end/event/${req.body.newsId}`,
     {
-      users_id: req.body.user_id,
+      user_id: req.body.user_id,
       event_id: req.body.newsId,
     },
   );
-  console.log(req.body.user_id, req.body.newsId);
-  res.json(data);
+  res.json(data, req.body.user_id, req.body.newsId);
 };
