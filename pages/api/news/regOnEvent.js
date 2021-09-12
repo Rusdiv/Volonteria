ec1620032147b78c9700b4773387e9e1a3e2addb
Rusdiv <ruslan.ru226@mail.ru>
2021-09-12 18:38:54 +0300

    #master testing post

--- pages/api/news/regOnEvent.js
+++ pages/api/news/regOnEvent.js
@@ -1,21 +1,23 @@
-import axios from 'axios';
-import FormData from 'form-data';
+// import axios from 'axios';
+// import FormData from 'form-data';
+
+const fetch = require('node-fetch');
 
 require('dotenv').config();
 
 export default async (req, res) => {
-  const formData = new FormData();
-  formData.append('user_id', req.body.user_id);
-  formData.append('event_id', req.body.newsId);
-  const request = await axios({
-    url: `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
-    method: 'POST',
-    data: formData,
-    headers: {
-      // Accept: 'application/json',
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  // const formData = new FormData();
+  // formData.append('user_id', req.body.user_id);
+  // formData.append('event_id', req.body.newsId);
+  // const request = await axios({
+  //   url: `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
+  //   method: 'POST',
+  //   data: formData,
+  //   headers: {
+  //     // Accept: 'application/json',
+  //     'Content-Type': 'multipart/form-data',
+  //   },
+  // });
   // const request = await axios.post(
   //   `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
   //   formData,
@@ -24,8 +26,13 @@ export default async (req, res) => {
   //     'Content-Type': 'multipart/form-data',
   //   },
   // );
-
-  console.log(formData, request);
-
-  res.json(1);
+  fetch(`${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`, {
+    method: 'POST',
+    body: new URLSearchParams({
+      user_id: req.body.user_id,
+      event_id: req.body.newsId,
+    }),
+  }).then(async (resp) => {
+    res.json(await resp.json());
+  });
 };
