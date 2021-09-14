8ab0b68a5ad155a9908213177037991e224cb456
Rusdiv <ruslan.ru226@mail.ru>
2021-09-14 14:24:16 +0300

    #master testing request reg

--- pages/api/news/regOnEvent.js
+++ pages/api/news/regOnEvent.js
@@ -7,7 +7,7 @@ dotenv.config();
 export default async (req, res) => {
   const formData = new FormData();
   formData.append('user_id', req.body.user_id);
-  formData.append('event_id', req.body.newsId);
+  formData.append('event_id', req.body.event_id);
   const { data } = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/prereg/event/${req.body.newsId}`,
     formData,
