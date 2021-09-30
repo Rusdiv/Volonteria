01cefa89254ddebeb056b5c3f65aa16c5ad453ff
Rusdiv <ruslan.ru226@mail.ru>
2021-09-30 21:59:05 +0300

    #master added sending emails

--- pages/Contacts.jsx
+++ pages/Contacts.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function Contacts() {
-  const handleSubmit = () => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
     axios.post('https://api.emailjs.com/api/v1.0/email/send', {
       service_id: 'service_c0h57cc',
       template_id: 'volonteria',
       user_id: 'user_tEiVSWn2tmbfhXnLFHj0G',
       template_params: {
-        name: 'Test',
-        email: 'example.com',
-        message: 'text',
+        name: { enteredName },
+        email: { enteredEmail },
+        message: { enteredMessage },
       },
     });
   };
 
   return (
-    <div>
-      <button type="button" onClick={handleSubmit}>
+    <form>
+      <input
+        type="email"
+        value={enteredEmail}
+        onChange={(e) => setEnteredEmail(e.target.value)}
+        placeholder="email"
+        required
+      />
+      <input
+        type="text"
+        value={enteredName}
+        onChange={(e) => setEnteredName(e.target.value)}
+        placeholder="name"
+        required
+      />
+      <input
+        type="text"
+        value={enteredMessage}
+        onChange={(e) => setEnteredMessage(e.target.value)}
+        placeholder="message"
+        required
+      />
+      <button type="submit" onClick={handleSubmit}>
         Отправить
       </button>
-    </div>
+    </form>
   );
 }
