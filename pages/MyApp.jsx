0836ea4b075a80bd35c4e4e6d8c0d3c83eb486a3
Rusdiv <ruslan.ru226@mail.ru>
2021-10-03 15:00:51 +0300

    #master added rules page

--- pages/MyApp.jsx
+++ pages/MyApp.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
 
 import Navigation from '../components/Navigation/Navigation';
 import AuthContext from '../store/auth-context';
 import LoginPage from './login';
+import Rules from './Rules';
 
 import styles from '../styles/Main.module.scss';
 import Header from '../components/Header/Header';
 
 function MyApp({ Component, pageProps }) {
   const authCtx = useContext(AuthContext);
+  const router = useRouter();
+
+  if (router.pathname === '/Rules') {
+    return <Rules />;
+  }
 
   if (!authCtx.isLoggedIn) {
     return <LoginPage />;
