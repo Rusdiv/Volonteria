93ad2b7ad662e5ca88d2c6cca7a14248b7cb838b
Rusdiv <ruslan.ru226@mail.ru>
2021-10-10 14:19:40 +0300

    #master

--- pages/api/news/endOnEvent.js
+++ pages/api/news/endOnEvent.js
@@ -1,23 +1,22 @@
 import axios from 'axios';
-import FormData from 'form-data';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default async (req, res) => {
-  const formData = new FormData();
-  formData.append('user_id', req.body.user_id);
-  formData.append('event_id', req.body.event_id);
   const { data } = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/end/event/${req.body.newsId}`,
-    formData,
-    { headers: formData.getHeaders() },
+    {
+      user_id: req.body.user_id,
+      event_id: req.body.newsId,
+    },
   );
 
   const pointData = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/add/points`,
-    formData,
-    { headers: formData.getHeaders() },
+    {
+      user_id: req.body.user_id,
+    },
   );
 
   res.json({ data, pointData });
