893b3c7493706f0fd10253b78e63983ca8a8e678
Rusdiv <ruslan.ru226@mail.ru>
2021-10-10 14:35:38 +0300

    #master testing

--- pages/api/news/endOnEvent.js
+++ pages/api/news/endOnEvent.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import querystring from 'querystring';
 
 dotenv.config();
 
 export default async (req, res) => {
   const { data } = await axios.post(
     `${process.env.CUSTOM_WP_API_URL}/end/event/${req.body.newsId}`,
-    {
+    querystring.stringify({
       user_id: req.body.user_id,
       event_id: req.body.newsId,
-    },
+    }),
   );
 
   const pointData = await axios.post(
