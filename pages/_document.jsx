f62d9db88c6a38ff52ff1c8fb5ec4e36fc4866c9
Rusdiv <ruslan.ru226@mail.ru>
2021-10-26 21:21:28 +0300

    #master added google analitics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -27,6 +29,22 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Neucha&display=swap"
             rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
