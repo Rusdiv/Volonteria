0d443b105895772113f3e8a14a4f3ef64a296085
rLukoyanov <rulukoyanov@gmail.com>
2022-08-03 21:32:40 +0300

    #students testing deploy

--- store/auth-context.jsx
+++ store/auth-context.jsx
@@ -10,7 +10,7 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState({});
 
   const logoutHandler = () => {
